dce51e67f9a2007c6a03bfb67f52f8b2bd164568
Atmostone <a.l.e.x.e.y.b@yandex.ru>
2021-01-20 20:57:37 +0900

    Added 'about' and 'contacts' flatpages

--- HotelBook/main/urls.py
+++ HotelBook/main/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 urlpatterns = [
+    path('pages/', include('django.contrib.flatpages.urls')),
     path('', views.index, name='index'),
 ]

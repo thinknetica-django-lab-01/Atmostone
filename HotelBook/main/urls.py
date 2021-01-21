5e4eed0bd239cdcf7f006c60fee4f42eebb42433
Atmostone <a.l.e.x.e.y.b@yandex.ru>
2021-01-21 18:16:22 +0900

    Added navbar for base template

--- HotelBook/main/urls.py
+++ HotelBook/main/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 
 from . import views
 
+app_name = 'main'
 urlpatterns = [
     path('pages/', include('django.contrib.flatpages.urls')),
     path('', views.index, name='index'),

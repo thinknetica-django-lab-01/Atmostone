ff590e92eb98e2f6bb4b15abebde2bf00fe2ae30
Atmostone <a.l.e.x.e.y.b@yandex.ru>
2021-01-30 10:09:03 +0300

    Added HotelList and HotelDetail views

--- HotelBook/apps/main/views.py
+++ HotelBook/apps/main/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
+from apps.main.models import Hotel
 
 
 def index(request):
@@ -13,3 +16,11 @@ def index(request):
         'is_owner': is_owner,
     }
     return render(request, 'main/index.html', context)
+
+
+class HotelList(ListView):
+    model = Hotel
+
+
+class HotelDetail(DetailView):
+    model = Hotel

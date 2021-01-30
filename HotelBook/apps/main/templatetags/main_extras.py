2be0fc50b3c2f44df6558fb4a2598704a58a5569
Atmostone <a.l.e.x.e.y.b@yandex.ru>
2021-01-30 09:04:36 +0300

    Created filter for reversing strings

--- HotelBook/apps/main/templatetags/main_extras.py
+++ HotelBook/apps/main/templatetags/main_extras.py
@@ -8,3 +8,8 @@ register = template.Library()
 @register.simple_tag
 def current_time():
     return datetime.datetime.now().time().isoformat(timespec='seconds')
+
+
+@register.filter
+def reverse(string):
+    return string[::-1]

a9e5b5ed5a482981dfb2b981d08fba979439a8f0
Atmostone <a.l.e.x.e.y.b@yandex.ru>
2021-01-30 17:58:32 +0300

    Making tags

--- HotelBook/apps/main/views.py
+++ HotelBook/apps/main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 
-from apps.main.models import Hotel
+from apps.main.models import Hotel, HotelFeature
 
 
 def index(request):
@@ -21,6 +21,16 @@ def index(request):
 class HotelList(ListView):
     model = Hotel
     paginate_by = 1
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['hotel_features'] = HotelFeature.objects.all()
+        return context
+
+    # def get_queryset(self):
+    #   print(self.kwargs)
+    #  self.features = get_object_or_404(HotelFeature, title=self.kwargs['features'])
+    # return Hotel.objects.filter(features=self.features)
 
 
 class HotelDetail(DetailView):

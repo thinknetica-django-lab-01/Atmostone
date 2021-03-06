9b7e8c604720d987a567896631dbe30a032358be
Atmostone <a.l.e.x.e.y.b@yandex.ru>
2021-03-06 10:56:57 +0300

    Fixed requested changes from 13.1

--- HotelBook/apps/main/middleware.py
+++ HotelBook/apps/main/middleware.py
@@ -2,10 +2,14 @@ import re
 
 
 class MobileMiddleware:
+    """
+    Middleware that checks if user uses mobile client
+    """
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
+        # regex for checking user agent containing mobile clients keywords
         mobile_agent_re = re.compile(r'.*(iphone|mobile|android)', re.IGNORECASE)
 
         if not mobile_agent_re.match(request.META['HTTP_USER_AGENT']):
